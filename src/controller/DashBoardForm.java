a4b515b556dbbbc67729188f006e770a102dd5c9
AsiriM2000 <asiriambagahawaththa@gmail.com>
2023-04-16 12:44:05 +0530

    content update

---
+++ src/controller/DashBoardForm.java
@@ -0,0 +1,23 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class DashBoardForm {
+    public void saveCustomerOnAction(ActionEvent actionEvent) {
+    }
+
+    public void searchCustomerOnAction(ActionEvent actionEvent) {
+    }
+
+    public void updateCustomerOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteCustomerOnAction(ActionEvent actionEvent) {
+    }
+
+    public void loadAllCustomersOnAction(ActionEvent actionEvent) {
+    }
+
+    public void placeOrderOnAction(ActionEvent actionEvent) {
+    }
+}

14d6c4b934e05bee744e88c3417032d65d2041ce
AsiriM2000 <asiriambagahawaththa@gmail.com>
2023-04-18 20:05:45 +0530

    content update

---
+++ src/controller/ItemCrud.java
@@ -0,0 +1,32 @@
+package controller;
+
+import model.Item;
+import util.SQLUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ItemCrud {
+    public static ArrayList<String> getItemCodes() throws SQLException, ClassNotFoundException {
+        ResultSet result = SQLUtil.executeQuery("SELECT code FROM Item");
+        ArrayList<String> codeList = new ArrayList<>();
+        while (result.next()) {
+            codeList.add(result.getString(1));
+        }
+        return codeList;
+    }
+
+    public static Item getItem(String code) throws SQLException, ClassNotFoundException {
+        ResultSet result = SQLUtil.executeQuery("SELECT * FROM Item WHERE code=?", code);
+        if (result.next()) {
+            return new Item(
+                    result.getString(1),
+                    result.getString(2),
+                    result.getString(3),
+                    result.getInt(4)
+            );
+        }
+        return null;
+    }
+}
